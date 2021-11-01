1c3cc3661a25ca6513e7e44bfcf271c5f0852638
Boris <borisatanasov2000@gmail.com>
2021-11-01 21:28:43 +0200

    first commit

---
+++ mysite/elsys/models.py
@@ -0,0 +1,14 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+
+# Create your models here.
+
+class Car(models.Model):
+    color = models.CharField(max_length=100)
+    made = models.DateTimeField(auto_now_add=True)
+    brand = models.CharField(max_length=50)
+    description = models.TextField()
+    yearProduction = models.IntegerField(default=2000, validators=[MinValueValidator(1), MaxValueValidator(2022)])
+    HP = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(1000)])
+    engine = models.CharField(max_length=50)
